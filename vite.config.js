7744d737b8abc8637bac479a9cd86c9fce48c30f
adam-dev2 <shaikadam252@gmail.com>
2025-04-21 03:03:38 +0530

    hopefully last commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   publicDir: 'public',
-  plugins: [
-    react()],
+  base: '/', // 👈 important for correct asset loading in production
+  plugins: [react()],
 })
